0f21a9ae96f3afd1e4f7b2a741341fa3c59cc5c1
Tyler French <tylerfrench89@gmail.com>
2020-06-18 15:37:43 -0700

    more work done

--- contract/client/src/components/pages/ConSearch.js
+++ contract/client/src/components/pages/ConSearch.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import '../../styles/ConSearch.css';
+import { useContractor } from '../../hooks'
 
 
 
 export default () => {
-  
+    const { contractor, getContractor } = useContractor()
+    // const { cart, addToCart } = useCart()
+    useEffect(() => {
+        getContractor()
+    }, [])
     return (
         <div>
             <div className="input-shelf">
@@ -12,13 +17,14 @@ export default () => {
             </div>
             <div className="contractor-shelf">
                 <div><img src="https://www.fillmurray.com/g/140/100" /></div>
-                <div>
+                <div className="info-shelf">
                     <div>Tyler French</div>
                     <div>A software developer in Las Vegas, Nevada. </div>
-                    <div></div>
+                    <div>developer, dj, event producer</div>
                 </div>
             </div>
         </div>
 
     )
 }
+
--- contract/client/src/redux/ducks/index.js
+++ contract/client/src/redux/ducks/index.js
@@ -1,2 +1,3 @@
 export { default as exampleState } from "./example"
 export { default as authState } from "./auth"
+export { default as contractorState } from "./ConSearch"
