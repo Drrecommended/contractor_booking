01c4bce10db957f6376a5a276aa4742db47f83d4
DivinaRose <divinarose123@gmail.com>
2020-06-18 20:36:48 -0700

    add new

--- contract/client/src/components/App.js
+++ contract/client/src/components/App.js
@@ -3,6 +3,7 @@ import '../styles/App.css';
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 import Routes from './Routes';
+
 export default () => <div>
   <Provider store={store}>
     <Routes />
--- contract/client/src/components/pages/ConProfile.js
+++ contract/client/src/components/pages/ConProfile.js
@@ -2,9 +2,15 @@ import React from 'react';
 import '../../styles/ConProfile.css';
 
 export default () => {
-  
+
     return (
-        <div></div>
+        <div className="contractor-profile">
+            <div className="contractor-logo">
+            </div>
+            <div className="contract-photo">
+                <img src="https://placeholder.com/50"></img>
+            </div>
+        </div>
 
     )
 }
