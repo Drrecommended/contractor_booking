3bc2a55dcc93bdf5cc6d8c727868d0e0797992f5
Markese Straughter <Kese08@hotmail.com>
2020-06-20 10:47:38 -0700

    initial

--- contract/client/src/components/pages/ConEdit.js
+++ contract/client/src/components/pages/ConEdit.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import '../../styles/ConEdit.css';
+import { Button, Input, Dropdown } from 'semantic-ui-react';
 
 export default () => {
 
   return (
-    <div>profile edit</div>
+  <div className="EditContainer">
+    <div className="UploadImg">
+
+    </div>
+    <div className="BusinessEdit">
+      <Input placeholder='Name' />
+      <Input placeholder='Business Name' />
+      <Dropdown placeholder='Trade 1' />  
+      <Dropdown placeholder='Trade 2'  />
+    </div>
+    <div className="BusinessLocationEdit">
+      <Input placeholder='Address' />
+      <Input placeholder='City, State' />
+      <Input placeholder='Zip Code' />
+    </div>
+        <div>
+          <Button positive>SAVE</Button>
+        </div>
+      
+  </div>
 
   )
 }
