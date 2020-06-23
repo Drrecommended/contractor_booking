d06865627019d11858ad6dbff8e8c08537d6d90b
Thomas Shannon <thomasshannon1117@gmail.com>
2020-06-23 15:05:29 -0700

    added getProfile method to grab user

--- contract/client/src/redux/ducks/auth/index.js
+++ contract/client/src/redux/ducks/auth/index.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import api from '../../../utils/request'
-import { AuthService }  from '../../../utils/request'
+import { AuthService } from '../../../utils/request'
 
 // 2. action definitions
 const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS'
@@ -11,55 +11,55 @@ const LOGOUT = 'auth/LOGOUT'
 
 // 3. initial state
 const initialState = {
-    example: '',
-    // on load get if user is authenticated
-    isAuthenticated: AuthService.isAuthenticated(),
-    pending: false,
+  example: '',
+  // on load get if user is authenticated
+  isAuthenticated: AuthService.isAuthenticated(),
+  pending: false,
 }
 
 // 4. reducer
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_PENDING:
-        return {...state, pending: true}
+      return { ...state, pending: true }
     case LOGIN_SUCCESS:
-        return {...state, isAuthenticated: true, pending: false}
+      return { ...state, isAuthenticated: true, pending: false }
     case LOGOUT:
-        return {...state, isAuthenticated: false, pending: false}
+      return { ...state, isAuthenticated: false, pending: false }
     default:
-        return state
+      return state
   }
 }
 
 function loginUser(username, password) {
-    return dispatch => {
-        return api.login(username, password)
-        .then(resp => {
-            dispatch({
-                type: LOGIN_SUCCESS,
-            })
+  return dispatch => {
+    return api.login(username, password)
+      .then(resp => {
+        dispatch({
+          type: LOGIN_SUCCESS,
         })
-    }
+      })
+  }
 }
 
 function logoutUser() {
-    return dispatch => {
-        return api.logout().then(resp => {
-            dispatch({
-                type: LOGOUT,
-            })
-        })
-    }
+  return dispatch => {
+    return api.logout().then(resp => {
+      dispatch({
+        type: LOGOUT,
+      })
+    })
+  }
 }
 
 function signupUser(username, password) {
-    return dispatch => {
-        return api.signup(username, password).then(resp => {
-            dispatch({
-                type: LOGOUT,
-            })
-        })
-    }
+  return dispatch => {
+    return api.signup(username, password).then(resp => {
+      dispatch({
+        type: LOGOUT,
+      })
+    })
+  }
 }
 
 // 6. custom hook
@@ -69,6 +69,7 @@ export function useAuth() {
   const login = (username, password) => dispatch(loginUser(username, password))
   const signup = (username, password) => dispatch(signupUser(username, password))
   const logout = () => dispatch(logoutUser())
+  const user = AuthService.getProfile()
   const testProtected = () => api.get('/dashboard')
 
   return { login, logout, signup, isAuthenticated, testProtected }
