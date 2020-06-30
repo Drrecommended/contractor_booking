6bcc54e693b61560b84409c14aba5de138747157
Tyler French <tylerfrench89@gmail.com>
2020-06-30 16:15:53 -0700

    navbar updates

--- contract/client/src/components/ui/Nav.js
+++ contract/client/src/components/ui/Nav.js
@@ -1,40 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import '../../styles/Nav.scss';
 import { Button, Icon } from 'semantic-ui-react'
-import { useContractor } from '../../hooks'
+import { useContractor, useAuth } from '../../hooks'
 import { Link } from 'react-router-dom'
 
 
 export default () => {
     const { contractors, getContractor } = useContractor()
+    const { isAuthenticated, logout } = useAuth()
     const [search, setSearch] = useState('')
     // const [ user, allowUser ]
     const handleSubmit = (e) => {
         e.preventDefault()
         getContractor(search)
     }
-
+    
     useEffect(() => {
         getContractor()
     }, [])
 
+
   return (
     <div id="header-style" class="ui clearing segment">
         <h2 class="ui left floated header">
-            <Link to="/profile"><button>Profile</button></Link>
-            <Link to="/search"><button>Search</button></Link>
+            <Link to="/profile">
+                <button className={isAuthenticated ? "show" : "hidden"}>Profile</button>
+            </Link>
+            <Link to="/search">
+                <button  className={isAuthenticated ? "show" : "hidden"}>Search</button>
+            </Link>
         </h2>
         <div>
-            <form className="hidden" onSubmit={handleSubmit}>
+            <form className={isAuthenticated ? "show" : "hidden"} onSubmit={handleSubmit}>
                 <input onChange={(e) => setSearch(e.target.value)}/> <button>SEARCH</button>
             </form>
         </div>
         <h2 className="ui right floated header">
-            <Link to="/order"><button>Order</button></Link>
-            <Link to="/login"><button>Log Out</button></Link>
+            <Link to="/order">
+                <button className={isAuthenticated ? "show" : "hidden"}>Order</button>
+            </Link>
+            <Link to="/login"><button onClick={logout} >Log Out</button></Link>
         </h2>
 
     </div>
 
   )
 }
+
