8d5d678411c39efa127a26a10cbb433c0417ffdd
Tyler French <tylerfrench89@gmail.com>
2020-07-01 13:04:30 -0700

    more navbar work

--- contract/client/src/components/ui/Nav.js
+++ contract/client/src/components/ui/Nav.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import '../../styles/Nav.scss';
 import { Button, Icon } from 'semantic-ui-react'
 import { useContractor, useAuth } from '../../hooks'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 export default () => {
     const { contractors, getContractor } = useContractor()
     const { isAuthenticated, logout } = useAuth()
     const [search, setSearch] = useState('')
+    const location = useLocation()
+
     // const [ user, allowUser ]
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -22,26 +24,33 @@ export default () => {
 
   return (
     <div id="header-style" class="ui clearing segment">
-        <h2 class="ui left floated header">
-            <Link to="/profile">
-                <button className={isAuthenticated ? "show" : "hidden"}>Profile</button>
-            </Link>
-            <Link to="/search">
-                <button  className={isAuthenticated ? "show" : "hidden"}>Search</button>
-            </Link>
-        </h2>
+        {
+            isAuthenticated ? 
+                <h2 class="ui left floated header">
+                    <Link to="/profile">
+                        <button>Profile</button>
+                    </Link>
+                    <Link to="/search">
+                        <button>Search</button>
+                    </Link>
+                </h2>  
+            : null 
+        }
         <div>
             <form className={isAuthenticated ? "show" : "hidden"} onSubmit={handleSubmit}>
                 <input onChange={(e) => setSearch(e.target.value)}/> <button>SEARCH</button>
             </form>
         </div>
-        <h2 className="ui right floated header">
-            <Link to="/order">
-                <button className={isAuthenticated ? "show" : "hidden"}>Order</button>
-            </Link>
-            <Link to="/login"><button onClick={logout} >Log Out</button></Link>
-        </h2>
-
+        {
+            isAuthenticated ? 
+                <h2 className="ui right floated header">
+                    <Link to="/order">
+                        <button>Order</button>
+                    </Link>
+                    <Link to="/login"><button onClick={logout} >Log Out</button></Link>
+                </h2>
+            : null 
+        }
     </div>
 
   )
