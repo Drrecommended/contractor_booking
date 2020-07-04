7016d7a6ec8416519095bf3dc7ec3845e2408c88
DivinaRose <divinarose123@gmail.com>
2020-07-03 22:15:35 -0700

    didn't save my duck, had to redo

---
+++ contract/server/router/profile.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const conn = require('../db.js')
+
+// conn.query(sql , [], (err, results, fields) => {})
+router.get('/profile', (req, res, next) => {
+  res.json({
+    thumbnail: "https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg_400x400.jpg",
+    first: "Prison",
+    last: "Mike",
+    address: {
+      street: "555 apple st",
+      city: "las vegas",
+      state: "NV"
+
+    },
+    trade: "Origami Paper Company",
+    BIO: "About Me",
+    options: [
+      { key: 1, text: 'cutting bushes', price: 20, value: 1 },
+      { key: 2, text: 'plumbing', price: 15, value: 2 },
+      { key: 3, text: 'renovation', price: 20, value: 3 },
+    ],
+
+  })
+})
+
+module.exports = router
