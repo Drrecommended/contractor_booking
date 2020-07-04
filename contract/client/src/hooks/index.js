dabfe946db44f8505c9138cbc6b3222501c53b16
Thomas Shannon <thomasshannon1117@gmail.com>
2020-07-04 14:33:06 -0700

    fixed casing issue on duck

--- contract/client/src/hooks/index.js
+++ contract/client/src/hooks/index.js
@@ -2,6 +2,7 @@ export { useExample } from "../redux/ducks/example"
 export { useAuth } from "../redux/ducks/auth"
 export { useContractor } from "../redux/ducks/ConSearch"
 export { useProfileIndex } from "../redux/ducks/ConProfileInfo"
+export { useEditProfile } from "../redux/ducks/conEdit"
 export { useOrder } from "../redux/ducks/ConOrder"
 export { useForm } from "./form"
 
