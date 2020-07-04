2fa502f0090ea3364cc909c2456738763c7458cd
Thomas Shannon <thomasshannon1117@gmail.com>
2020-07-04 12:19:22 -0700

    fixed issue with sql string for fuzzy search in constractor searchD

--- contract/server/router/conSearch.js
+++ contract/server/router/conSearch.js
@@ -4,23 +4,24 @@ const conn = require('../db.js')
 
 
 router.get('/contractor-search', (req, res, next) => {
-  console.log(req.query)
-  const sql = 
-    'SELECT p.bio, u.first_name, u.last_name, s.description ' +
-    'FROM contractor_app.services s ' + 
-    'INNER JOIN users u ON s.user_id = u.id ' + 
-    'INNER JOIN profiles p ON p.id = u.profile_id ' + 
-    'WHERE description LIKE trim bushes;' 
-    conn.query(
-      sql, 
-      [req.query.word],
-      (err, results, fields) => {
-        console.log(err)
-        console.log(results)
-        res.json(results)
-      })
+  // TODO: quotes with percentage and ? in backtick string not working...
+  const fuzzySearchTerm = "%" + req.query.word + "%"
+  const sql = `
+    SELECT p.bio, u.first_name, u.last_name, s.description
+    FROM contractor_app.services s
+    INNER JOIN users u ON s.user_id = u.id
+    INNER JOIN profiles p ON p.id = u.profile_id
+    WHERE s.description LIKE ?`
+  conn.query(
+    sql,
+    [fuzzySearchTerm],
+    (err, results, fields) => {
+      console.log(err)
+      console.log(results)
+      res.json(results)
+    })
 })
 
 
-  
-  module.exports = router
+
+module.exports = router
