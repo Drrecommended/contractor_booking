94fab0946e604373a6a32a3c7e3c8a22ce156bc3
Thomas Shannon <thomasshannon1117@gmail.com>
2020-07-08 20:14:37 -0700

    updated node module

--- contract/client/src/components/pages/Sandbox.js
+++ contract/client/src/components/pages/Sandbox.js
@@ -1,18 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from 'semantic-ui-react'
-import { useForm } from '../../hooks'
+import React, { useEffect } from 'react';
+import { Button, Input } from 'semantic-ui-react'
+import { useForm, useExample } from '../../hooks'
 
 export default (props) => {
-  const [isTrue, setTrue] = useState(false)
-
+  const [exampleForm, setExampleForm, resetForm, setFormTo] = useForm({ first: '', last: '' })
+  const { getExample } = useExample()
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(exampleForm)
+    resetForm()
+  }
+  function populateFormWithDataFromBackend() {
+    getExample().then(resp => {
+      // populate form with data from backend
+      setFormTo(resp.user)
+    })
+  }
   return (
     <div className="Dash">
-      {
-        isTrue ?
-          <button style={{ padding: '20px' }} onClick={() => setTrue(!isTrue)}>it is true</button>
-          :
-          <button style={{ padding: '20px' }} onClick={() => setTrue(!isTrue)}>it is false</button>
-      }
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="first"
+          value={exampleForm.first}
+          onChange={setExampleForm}
+        />
+        <Input
+          type="text"
+          name="last"
+          value={exampleForm.last}
+          onChange={setExampleForm}
+        />
+        <Button type="submit">submit</Button>
+      </form>
+      <Button style={{ margin: '20px' }} onClick={() => populateFormWithDataFromBackend()}>set form programatically from data in backend</Button>
     </div>
   )
 }
