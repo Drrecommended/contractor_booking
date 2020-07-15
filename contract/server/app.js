0325731d419aa9dd5935a844811bb2f9da9a2ab5
thomas1117 <thomasshannon117@yahoo.com>
2020-07-15 11:50:23 -0700

    switched port back

--- contract/server/app.js
+++ contract/server/app.js
@@ -33,6 +33,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 
-app.listen(80, () => {
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
