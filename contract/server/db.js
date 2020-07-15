c26cae2c367afef30bc7e28a7c402c9def6e1f12
thomas1117 <thomasshannon117@yahoo.com>
2020-07-15 15:03:53 -0700

    fixed script

--- contract/server/db.js
+++ contract/server/db.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2')
 const config = require('config')
+require('dotenv').config()
 
-const pool = mysql.createPool({
+const connObj = {
   connectionLimit: 10,
   host: config.get('db.host'),
   user: config.get('db.user'),
   password: config.get('db.password'),
   database: config.get('db.database')
-})
-
+}
+const connection = process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : connObj
+const pool = mysql.createPool(process.env.JAWSDB_URL)
 module.exports = pool
