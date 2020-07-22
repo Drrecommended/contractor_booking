5b505a421889e928b290acc7bd746e47cb9247f7
DivinaRose <divinarose123@gmail.com>
2020-07-21 19:59:59 -0700

    finish empty gallery and avatar

--- contract/client/src/components/ui/Avatar.js
+++ contract/client/src/components/ui/Avatar.js
@@ -4,15 +4,15 @@ import React from "react"
 
 
 export default (props) => {
-  
+
   return (
     <div
       style={{
-        backgroundImage: `url(${props.image})`,
+        backgroundImage: `url(${props.image || 'https://a4-images.myspacecdn.com/images04/3/509cbf67b75245eeb8e2fd365652b9a7/300x300.jpg'})`,
         height: (props.size || 200) + "px",
         width: (props.size || 200) + "px",
         backgroundSize: "cover",
-        borderRadius: "50%",
+        borderRadius: props.type == 'square' ? 0 : '50%'
       }}
     ></div>
   )
