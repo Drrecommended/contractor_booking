a0ed9ebb7e6a0956254d887ff9a4745115d9f65c
thomas1117 <thomasshannon117@yahoo.com>
2020-07-22 10:08:58 -0700

    addd start of available dates

--- contract/server/router/profile.js
+++ contract/server/router/profile.js
@@ -58,5 +58,35 @@ router.get('/profile/:id', async (req, res, next) => {
   res.json(profile)
 })
 
+router.get('/available/:id', async (req, res, next) => {
+  const sub = `
+  select * from 
+    (select adddate('1970-01-01',t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i) selected_date from
+     (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+     (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+     (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
+     (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
+     (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
+    where selected_date between '2020-05-22' and '2020-06-23'
+  `
+
+  const [results] = await conn.promise().query(`
+  SELECT FORMAT(date, 'YYYY-MM-DD') as date FROM orders
+  WHERE date IN (${sub}) AND contractor_id = ?;
+  `, [req.params.id])
+  
+  const dateCache = {}
+  results.map(item => {
+    console.log(item.date)
+    dateCache[item.date] = true
+  })
+
+  const [range] = await conn.promise().query(sub)
+  const available = range.map(item => {
+    return {date: item.selected_date, available: !dateCache[item.selected_date]}
+  })
+  res.json(available)
+})
+
 
 module.exports = router
