d360eb379b35381ba3c52e925acdfdbe69a2ec46
thomas1117 <thomasshannon117@yahoo.com>
2020-07-23 20:02:07 -0700

    added back detail page

--- contract/client/src/components/pages/ConSearch.js
+++ contract/client/src/components/pages/ConSearch.js
@@ -29,7 +29,7 @@ export default () => {
         <div className="searchPage">
           {contractors.map((contractor) => {
             return (
-              <Link className="search-link" to={`/profile/${contractor.profile_id}`}>
+              <Link className="search-link" to={`/profile/detail/${contractor.profile_id}`}>
                 <div className="contractor-shelf">
                   <div className="conSearchImage">
                     <img src={contractor.thumbnail} />
